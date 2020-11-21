0ad27a5038ef43bb7a60f9ca065353afa7827e39
Martin Luzuriaga <luzuriagamartin@gmail.com>
2020-11-21 00:38:11 -0300

    actualizacion

---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function List(props){
+  return(
+    <h1>Mensaje de React!!</h1>
+  );
+
+}
+
+export default List;
+
+
+
+    
---
+++ src/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Menu(props){
+  return(
+    <div className="container">
+      <div className="subcontainer">
+        <div className="logo">
+
+        </div>
+
+        <div className="search">
+        </div>
+
+        <div className="actions">
+        
+        </div>
+ 
+      </div>
+    </div>
+  
+  );
+}
+
+export default Menu;
